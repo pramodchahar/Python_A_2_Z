5c12d73442c9eb1848a0704919f60c691d56d031
pramodchahar <pramod.chahar@gmail.com>
2018-10-04 05:32:58 +0530

    Updated the logic of case senstivity

--- simple_apps/AI_game.py
+++ simple_apps/AI_game.py
@@ -3,34 +3,34 @@ import random
 ai_num=random.randint(1,3)
 computer=[]
 if ai_num == 1:
-    computer='Rock'
+    computer='rock'
 elif ai_num ==2:
-    computer="Paper"
+    computer="paper"
 else:
-    computer='Scissors'
+    computer='scissors'
 
-player_1=input("Player 1 , Make your input : ")
+player_1=input("Player 1 , Make your input : ").lower()
 print(f"Computer choses : {computer}")
 
 if player_1 == computer:
     print(" Game is a Tie !! ")
 
-elif player_1 =='Rock':
-    if computer =='Scissors':
+elif player_1 =='rock':
+    if computer =='scissors':
         print("Player 1 wins !!")
-    elif computer =='Paper':
+    elif computer =='paper':
         print("Computer wins !!")
 
-elif player_1 =='Scissors':
-    if computer =='Paper':
+elif player_1 =='scissors':
+    if computer =='paper':
         print("Player 1 wins !!")
-    elif computer =='Rock':
+    elif computer =='rock':
         print("Computer wins !!")
 
-elif player_1 =='Paper':
-    if computer =='Rock':
+elif player_1 =='paper':
+    if computer =='rock':
         print("Player 1 wins !!")
-    elif computer =='Scissors':
+    elif computer =='scissors':
         print("Computer wins !!")
 else:
     print("Something went wrong  !!!!")
