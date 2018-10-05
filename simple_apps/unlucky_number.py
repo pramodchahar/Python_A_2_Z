e8f386945c0805f724266d768089adaf3a42c169
pramodchahar <pramod.chahar@gmail.com>
2018-10-06 04:07:27 +0530

    unlucky numbers script

---
+++ simple_apps/unlucky_number.py
@@ -0,0 +1,10 @@
+for i in range (1,21):
+    if i ==4 :
+        print('The number is unlucky !!')
+    elif  i==13:
+        print('The number is unlucky !!')
+    elif i %2 ==1:
+        print("The number is odd !! ")
+    else :
+        print("The number is even !!")
+
