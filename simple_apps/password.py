ddc4f01a49d15663e2e1c1a3dbb873385fe1306f
pramodchahar <pramod.chahar@gmail.com>
2018-10-06 04:18:39 +0530

    while loop

---
+++ simple_apps/password.py
@@ -0,0 +1,5 @@
+msg=input('Please the magic word !!! ')
+while msg !='Hazard':
+    print("Wrong Password , please try again !!")
+    msg=input('Please the magic word !!! ')
+print("THis time its correct !! Happy surfing ")
